81dec4881fe346e5fddda752f30256bc0f695ed5
David Geronazzo <david.geronazzo@zilioenvironment.com>
2024-08-26 17:07:12 +0200

    UPDATE

--- PortaleImpianti_project/views.py
+++ PortaleImpianti_project/views.py
@@ -11,12 +11,10 @@ def loadingPageMonitoraggio(request):
 	return render(request, 'loading_gif/loading_1.html')
 
 
-@login_required(login_url='login')
 def loadingPageAnalisi(request):
 	return render(request, 'loading_gif/loading_2.html')
 
 
-@login_required(login_url='login')
 def loadingPageCorrispettivi(request):
 	link_corrispettivi = linkportale.objects.filter(tag='portale-corrispettivi')[0].link
 	return render(request, 'loading_gif/loading_3.html', {'link_corrispettivi': link_corrispettivi})
