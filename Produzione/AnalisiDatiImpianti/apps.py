cb8acea1cdc004c64bff5d492bc6ef2917a3024c
Giulio Lazzarozilio <giulio.lazzaro@zilioenvironment.com>
2025-10-07 15:11:49 +0200

    07/10/2025

--- Produzione/AnalisiDatiImpianti/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnalisidatiimpiantiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AnalisiDatiImpianti'
--- Produzione/MonitoraggioImpianti/scripts/export_impianti.py
+++
@@ -1,8 +0,0 @@
-from MonitoraggioImpianti.models import *
-import pandas as pd
-
-
-if __name__=='__main__':
-	impianti = Impianto.objects.values()
-	df = pd.DataFrame(impianti)
-	df.to_excel('Impianti.xlsx', index=False)
