5e0e2a3dbc90d7fd36bb6a172db062901d13e8d5
LucaParise <luca.parise@zilioenvironment.com>
2026-03-18 12:12:04 +0100

    Refactor PortaleZilioService home UI: remove legacy modal form flow, add table action placeholders, and clean up table interactions

--- Produzione/PortaleZilioService/urls.py
+++ Produzione/PortaleZilioService/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
 from .views import (
-    fv_clienti_create_view,
-    fv_in_costruzione_create_view,
-    fv_ppu_create_view,
-    fv_proprieta_create_view,
     home_view,
     sync_isc_metrics_view,
     test_view,
@@ -14,10 +10,6 @@ app_name = "PortaleZilioService"
 
 urlpatterns = [
     path("", home_view, name="home"),
-    path("fv/new/clienti/", fv_clienti_create_view, name="fv-clienti-create"),
-    path("fv/new/proprieta/", fv_proprieta_create_view, name="fv-proprieta-create"),
-    path("fv/new/in-costruzione/",fv_in_costruzione_create_view,name="fv-in-costruzione-create",),
-    path("fv/new/ppu/", fv_ppu_create_view, name="fv-ppu-create"),
     path("api/sync-isc/", sync_isc_metrics_view, name="sync-isc"),
     path("test/", test_view, name="test-view"),
 ]
